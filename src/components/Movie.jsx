902fa2707656ffdcea34c64588314d278da7b66f
kylewrood <137108769+kylewrood@users.noreply.github.com>
2023-09-13 21:49:12 -0500

    merged the changes with animations

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
-<<<<<<< HEAD
-import { motion } from "framer-motion";
-
-=======
 import { Heart } from 'lucide-react';
 import { useAppContext } from '../utils/AppContext';
->>>>>>> main
+import { motion } from "framer-motion";
 const Movie = ({movie}) => {
     //artwork title description year
 
@@ -14,7 +10,7 @@ const Movie = ({movie}) => {
     <div className=' bg-slate-100 w-[250px] max-h-[400px] h-[400px] shadow-lg rounded-lg'>
         <motion.div className='box'whileHover={{ scale: 1.2 }}
       whileTap={{ scale: 0.9 }}
-      transition={{ type: "spring", stiffness: 400, damping: 17 }}>
+      transition={{ type: "spring", stiffness: 400, damping: 17 }}/>
             <img src={movie?.Poster} className='rounded-t-lg object-cover overflow-hidden w-full h-450px]'/>
           <div className=' p-2 space-y-2'>
           <h2 className='font-bold text-base'>Title: {movie?.Title}</h2>
@@ -29,13 +25,9 @@ const Movie = ({movie}) => {
             </div>
             
           </div>
-<<<<<<< HEAD
-        </motion.div>
-=======
         
         </div>
->>>>>>> main
-    </div>
+  
   )
 }
 
