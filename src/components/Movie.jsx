f1078a6438cf7fb1e9f3ab3c3cc5cbce0b238102
Kyle Rood <kylewrood@gmail.com>
2023-09-14 13:46:20 -0500

    Committing due to cant switch branches to animations

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,12 +1,6 @@
 import React from 'react'
-<<<<<<< HEAD
-import { Heart } from 'lucide-react';
-import { useAppContext } from '../utils/AppContext';
-import { motion } from "framer-motion";
-=======
 import { motion } from "framer-motion";
 
->>>>>>> animations
 const Movie = ({movie}) => {
     //artwork title description year
 
@@ -15,7 +9,7 @@ const Movie = ({movie}) => {
     <div className=' bg-slate-100 w-[250px] max-h-[400px] h-[400px] shadow-lg rounded-lg'>
         <motion.div className='box'whileHover={{ scale: 1.2 }}
       whileTap={{ scale: 0.9 }}
-      transition={{ type: "spring", stiffness: 400, damping: 17 }}/>
+      transition={{ type: "spring", stiffness: 400, damping: 17 }}>
             <img src={movie?.Poster} className='rounded-t-lg object-cover overflow-hidden w-full h-450px]'/>
           <div className=' p-2 space-y-2'>
           <h2 className='font-bold text-base'>Title: {movie?.Title}</h2>
@@ -24,15 +18,9 @@ const Movie = ({movie}) => {
             <h2 className='font-semibold text-slate-600 '>Type: {movie?.Type}</h2>
             <h2 className='font-semibold text-slate-600'> Year: {movie?.Year}</h2>
           </div>
-<<<<<<< HEAD
-        
-        </div>
-  
-=======
-
+          
         </motion.div>
     </div>
->>>>>>> animations
   )
 }
 
