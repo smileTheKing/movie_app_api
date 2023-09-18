5fadc39beee4d1605a63cead9a25a1f4e9e9bf00
LeoSOSAPONS <137127243+LeoSOSAPONS@users.noreply.github.com>
2023-09-18 18:01:13 -0500

    few style changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { useAppContext } from "../utils/AppContext";
 import { motion } from "framer-motion";
 
 const Navbar = () => {
-  const { getMovieByTitle, search, setSeacrh,getMovies  } = useAppContext();
+  const { getMovieByTitle, search, setSearch,getMovies  } = useAppContext();
 
   const handleSearch = async () => {
     if (search) {
@@ -15,23 +15,23 @@ const Navbar = () => {
 
   const handleClear = async () => {
    
-      setSeacrh("")
+      setSearch("")
       getMovies(1)
       
   }
 
   return (
     <>
-      <nav className="bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-700">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <nav className="bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-blue-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-.5">
           <Link to="/" className="flex items-center">
             <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8 mr-3"
+              src="src/assets/movie-app-logo.png"
+              className="h-24 mr-3"
               alt=" Logo"
             />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
-              Movie
+            <span className="font-cinzel self-center text-2xl font-semibold whitespace-nowrap text-white">
+              MovieNook
             </span>
           </Link>
           <div
@@ -41,7 +41,7 @@ const Navbar = () => {
           >
             <input
               value={search}
-              onChange={(e) => setSeacrh(e.target.value)}
+              onChange={(e) => setSearch(e.target.value)}
               className="flex-1 p-2 outline-none bg-inherit text-center"
             />
             <button
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -6,7 +6,7 @@ const SideBar = () => {
   return (
     <div className='  min-h-full min-w-[300px] text-slate-100  p-8 justify-start items-center md:flex flex-col bg-slate-700'>
         <h2 className=' font-bold text-3xl mt-4'> Genre</h2>
-        <hr className='border-slate-500 w-full my-4 ' />
+        <hr className='border-gray-900 w-full my-4 ' />
         <ul  className='mt-8 space-y-6 flex flex-col items-center text-slate-300 justify-center font-semibold '>       
             <Link> <motion.li
             whileHover={{ scale: 2.0 }}
