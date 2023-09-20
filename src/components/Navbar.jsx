9e3003be4b9ad2cd50df8dd5c91c8654f69f6aeb
kylewrood <137108769+kylewrood@users.noreply.github.com>
2023-09-19 21:39:09 -0500

    add navbar committ

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -109,7 +109,8 @@ const Navbar = () => {
             id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 pl-3 pr-4 text-white-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Dropdown <svg className="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
   </svg></motion.button>
-  {/* <!-- Dropdown menu --> */}
+  {/* <!-- Dropdown menu --> */} 
+  {/* nav dropdown to the next line to click to expand */}
   <div id="dropdownNavbar" className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
         <ul className="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
             <li>
